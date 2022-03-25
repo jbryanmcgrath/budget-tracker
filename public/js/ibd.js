16c4af2acf2c1fcfd66956114f429224953bb4ae
jbryanmcgrath <90098243+jbryanmcgrath@users.noreply.github.com>
2022-03-25 12:40:38 -0400

    testing for heroku

---
+++ public/js/ibd.js
@@ -0,0 +1,61 @@
+let db;
+
+const request = indexedDB.open("budget_tracker", 1);
+
+request.onupgradeneeded = function (event) {
+    const db = event.target.result;
+    db.createObjectStore('new_budget', { atuoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+    db = event.target.result;
+    if (navigator.onLine) {
+        uploadBudget();
+    }
+};
+
+request.onerror = function (event) {
+    console.log("Error: " + event.target.errorCode);
+};
+
+function saveRecord(record) {
+    const transaction = db.transaction(["new_budget"], "readwrite");
+
+    const store = transaction.objectStore("new_budget");
+
+    store.add(record);
+}
+
+function sendBudget() {
+    const transaction = db.transaction(["new_budget"], "readwrite");
+
+    const store = transaction.objectStore("new_budget");
+
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+                .then(response => response.json())
+                .then(() => {
+                    const transaction = db.transaction(["new_budget"], "readwrite");
+                    const store = transaction.objectStore("new_budget");
+                    store.clear();
+                });
+        }
+    };
+}
+function deletePending() {
+    const transaction = db.transaction(["new_budget"], "readwrite");
+    const store = transaction.objectStore("new_budget");
+    store.clear();
+}
+
+window.addEventListener("online", sendBudget);
